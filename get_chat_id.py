9edad9a715edd99716641be5af020bd513add1a6
yuval yakuti <yuval.yakuti@post.runi.ac.il>
2025-04-08 17:54:42 +0300

    fix typo

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,11 +1,21 @@
 import asyncio
 from telegram import Bot
+from telegram.constants import ParseMode
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = Bot(token=bot_token)
 
 async def main():
-    bot = Bot(token="8010873044:AAEs9_mZ53Jazft6-v3UoiH7O-cr4xoBQng")
     updates = await bot.get_updates()
     for update in updates:
-        print(update.message.chat.id)
+        print(f"Chat ID: {update.message.chat.id}")
+        print(f"Username: {update.message.chat.username}")
+        print(f"Full Name: {update.message.chat.full_name}")
+        print("-----------")
 
 if __name__ == "__main__":
     asyncio.run(main())
